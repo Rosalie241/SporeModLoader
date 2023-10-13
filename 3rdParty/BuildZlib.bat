7a1374fc349fe9c401d72553b42c911cd2679a3a
Rosalie Wanders <rosalie@mailbox.org>
2023-10-13 14:38:31 +0200

    3rdParty: correct BuildZlib.bat to only build zlibstatic

--- 3rdParty/BuildZlib.bat
+++ 3rdParty/BuildZlib.bat
@@ -3,7 +3,6 @@ set ZLIB_CUR_DIR=%~dp0
 
 set ZLIB_DIR=%ZLIB_CUR_DIR%\zlib
 
-rmdir /S /Q "%ZLIB_DIR%\build"
 mkdir "%ZLIB_DIR%\build"
 
 set PATH=%PATH%;C:\Program Files\CMake\bin
@@ -11,4 +10,4 @@ set PATH=%PATH%;C:\Program Files\CMake\bin
 cmake -S "%ZLIB_DIR%" -B "%ZLIB_DIR%\build" -A Win32 ^
 	-DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded
 
-cmake --build "%ZLIB_DIR%\build" --config Release
+cmake --build "%ZLIB_DIR%\build" --config Release --target zlibstatic
