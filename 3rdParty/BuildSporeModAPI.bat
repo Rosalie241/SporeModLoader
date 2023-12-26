e4142149ace4ca5528ea2d2a551694a76cea3978
Rosalie Wanders <rosalie@mailbox.org>
2023-12-26 13:42:04 +0100

    3rdParty: set BuildVer manually in BuildSporeModAPI.bat

--- 3rdParty/BuildSporeModAPI.bat
+++ 3rdParty/BuildSporeModAPI.bat
@@ -2,13 +2,9 @@
 
 set SPOREMODAPI_CUR_DIR=%~dp0
 
-for /F "usebackq" %%F in (`git --no-pager --git-dir %SPOREMODAPI_CUR_DIR%/Spore-ModAPI/.git log --oneline ^| find "" /v /c`) do (
-	set SPOREMODAPI_COMMITCOUNT=%%F
-)
-
 msbuild "%SPOREMODAPI_CUR_DIR%\Spore-ModAPI\Spore ModAPI" ^
 	/t:BuildDlls ^
-	/p:BuildVer=%SPOREMODAPI_COMMITCOUNT% ^
+	/p:BuildVer=291 ^
 	/p:Config=Release ^
 	/p:Platform=Win32 ^
 	/m
