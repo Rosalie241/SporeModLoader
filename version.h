5414cd28ed6f1ce5356071764689332f696dfe57
Rosalie Wanders <rosalie@mailbox.org>
2023-12-27 13:20:31 +0100

    Add version.h

---
+++ version.h
@@ -0,0 +1,13 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+#define STR(x) #x
+#define STR_MACRO(x) STR(x)
+
+#define VERSION_MAJOR 3
+#define VERSION_MINOR 1
+#define VERSION_BUILD 0
+
+#define VERSION_STR STR_MACRO(VERSION_MAJOR) "." STR_MACRO(VERSION_MINOR) "." STR_MACRO(VERSION_BUILD)
+
+#endif // VERSION_H
