1fcc0142316f7a4bc97f0576fdd815db63ba124c
Rosalie Wanders <rosalie@mailbox.org>
2023-12-30 02:21:00 +0100

    3rdParty: update Spore-ModAPI to v2.5.292

--- 3rdParty/BuildSporeModAPI.bat
+++ 3rdParty/BuildSporeModAPI.bat
@@ -4,7 +4,7 @@ set SPOREMODAPI_CUR_DIR=%~dp0
 
 msbuild "%SPOREMODAPI_CUR_DIR%\Spore-ModAPI\Spore ModAPI" ^
 	/p:Configuration="Release DLL" ^
-	/p:SDK_BUILD_VER=291 ^
+	/p:SDK_BUILD_VER=292 ^
 	/p:EXECUTABLE_TYPE=10 ^
 	/p:Platform=Win32 ^
 	/m
