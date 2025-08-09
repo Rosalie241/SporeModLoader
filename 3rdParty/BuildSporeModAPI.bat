4e4b5d2d7b2ea7b606a21cf39f81f649bb7cdd0c
Rosalie Wanders <rosalie@mailbox.org>
2025-08-09 14:33:26 +0200

    3rdParty: build Spore-ModAPI legacy DLLs

--- 3rdParty/BuildSporeModAPI.bat
+++ 3rdParty/BuildSporeModAPI.bat
@@ -1,10 +1,17 @@
 @echo off
 
 set SPOREMODAPI_CUR_DIR=%~dp0
+set VERSION=400
 
 msbuild "%SPOREMODAPI_CUR_DIR%\Spore-ModAPI\Spore ModAPI" ^
 	/p:Configuration="Release DLL" ^
-	/p:SDK_BUILD_VER=400 ^
+	/p:SDK_BUILD_VER=%VERSION% ^
 	/p:EXECUTABLE_TYPE=10 ^
 	/p:Platform=Win32 ^
+	/m
+
+msbuild "%SPOREMODAPI_CUR_DIR%\Spore-ModAPI\Spore ModAPI Legacy" ^
+	/p:Config=Release ^
+	/t:BuildLegacyDlls ^
+	/p:BuildVer=%VERSION% ^
 	/m
